3508d34cf7f8f1cada6defaecc29c217b3da128e
harshpradhan44 <harsh.pradhan44@gmail.com>
2023-12-22 04:04:56 +0530

    implement custom error

--- errors/missingParam.go
+++ errors/missingParam.go
@@ -1,10 +1,12 @@
 package errors
 
+import "fmt"
+
 type MissingParamsError struct {
 	Code   int
 	Params []string
 }
 
 func (err MissingParamsError) Error() {
-
+	fmt.Printf("missing param(s) : %v", err.Params)
 }
